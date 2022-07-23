559f66cbef987268733b84120d179d101985ed41
Jarno Beeckman <99101540+Blitzhaxx@users.noreply.github.com>
2022-07-23 03:13:44 +0200

    fixed order

--- src/repository/mod.js
+++ src/repository/mod.js
@@ -1,7 +1,7 @@
 const { tables, getKnex } = require('../data');
 
 const getAll = async () => {
-  return await getKnex()(tables.mod).select("id","name","author").orderBy('id', 'desc');
+  return await getKnex()(tables.mod).select("id","name","author").orderBy('id', 'asc');
 };
 
 const getById = async (a) => {
